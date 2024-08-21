ff986e0850c04919829eb8bad960367c292bd520
Lenício Jr <lenicio.junior@hotmail.com>
2024-08-21 08:53:53 -0300

    Inclusão de ação JS

--- 21-08-2024/Introdução Ao Javascript/script.js
+++ 21-08-2024/Introdução Ao Javascript/script.js
@@ -1,1 +1,11 @@
-alert("Hello World!");
+document.getElementById('btn-enviar')
+.addEventListener('click', function(e) {
+  e.preventDefault(); // Anula o comportamento padrão de submissão do formulário
+  
+  let nome = document.getElementById('input-nome').value;
+  alert("O nome é: " + nome);
+
+})
+
+
+
