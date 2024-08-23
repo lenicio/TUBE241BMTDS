d7b01042f919c6cdd021c0c339801d67194a82b5
Lenício Jr <lenicio.junior@hotmail.com>
2024-08-23 09:32:06 -0300

    Exercício contador de cliques

---
+++ 23-08-2024/Atividade Contador de Cliques/script.js
@@ -0,0 +1,9 @@
+let contador = 0;
+
+document.getElementById('input_botao').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  contador ++;
+  document.getElementById('resultado').innerText = contador;
+
+})
